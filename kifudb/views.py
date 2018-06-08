174074565dadfb059f7784607f6b20578698f6bb
mateifl <sadyc357>
2018-06-08 17:02:07 +0300

    working on views

--- kifudb/views.py
+++ kifudb/views.py
@@ -8,10 +8,10 @@ def load_games(load_type = None):
         return Kifu.objects.all()[:20]
     elif load_type[0] == 'Group':
         print("loading games from " + load_type[1])
-        return Kifu.objects.filter(groups__name = load_type[1])
+        return Kifu.objects.filter(groups__slug = load_type[1])
     elif load_type[0] == 'Tag':
         print("loading games with tag " + load_type[1])
-        return Kifu.objects.filter(tag__name = load_type[1])
+        return Kifu.objects.filter(tag__slug = load_type[1])
 
 
 class BaseListView(TemplateView):
@@ -27,6 +27,8 @@ class BaseListView(TemplateView):
             kifus = load_games(('Group', self.kwargs['group_name']))
         if 'tag_name' in self.kwargs:
             kifus = load_games(('Tag', self.kwargs['tag_name']))
+
+        print("Found " + str(len(kifus)) + " games")
         context['KIFUS'] = kifus
 
         groups = KifuGroup.objects.all()
@@ -39,17 +41,6 @@ class GameView(DetailView):
     template_name = "game_view.html"
     model = Kifu
 
-
-class FrontPageView(TemplateView):
-    template_name = "front.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(FrontPageView, self).get_context_data(**kwargs)
-        blog_posts = Kifu.objects.all()
-        groups = KifuGroup.objects.all()
-        context['KIFUS'] = blog_posts
-        context['GROUPS'] = groups
-        return context
 
 
 class TestGameView(TemplateView):
